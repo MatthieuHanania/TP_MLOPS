dd41a1628d17b3146f467da224e206f424b584ce
MatthieuHanania <reycun18@gmail.com>
2022-11-29 20:35:23 +0100

    MLOPS

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //url: "mongodb+srv://user1:pass123@cluster0.nrbf5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  url :"mongodb+srv://matthieu:1234@cluster0.b0tjex0.mongodb.net/test"
+};
